bbf1c6536137324af758b2fbf16580b24608e292
0xReki <mail@0xreki.de>
2016-12-11 00:57:17 +0100

    added automatic scaling to canvas size; meter width derived from number of meters

--- js/html5_audio_visualizer.js
+++ js/html5_audio_visualizer.js
@@ -156,12 +156,12 @@ Visualizer.prototype = {
         var that = this,
             canvas = document.getElementById('canvas'),
             cwidth = canvas.width,
-            cheight = canvas.height - 2,
-            meterWidth = 10, //width of the meters in the spectrum
-            gap = 2, //gap between meters
             capHeight = 2,
-            capStyle = '#fff',
-            meterNum = 800 / (10 + 2), //count of the meters
+            cheight = canvas.height - capHeight,
+            capStyle = '#00f',
+            meterNum = 128, //count of the meters
+            gap = 2, //gap between meters
+            meterWidth = (cwidth - gap) / meterNum - gap, //width of the meters in the spectrum
             capYPositionArray = []; ////store the vertical position of hte caps for the preivous frame
         ctx = canvas.getContext('2d'),
         gradient = ctx.createLinearGradient(0, 0, 0, 300);
@@ -188,7 +188,7 @@ Visualizer.prototype = {
             var step = Math.round(array.length / meterNum); //sample limited data from the total array
             ctx.clearRect(0, 0, cwidth, cheight);
             for (var i = 0; i < meterNum; i++) {
-                var value = array[i * step];
+                var value = array[i * step] * cheight / 255;
                 if (capYPositionArray.length < Math.round(meterNum)) {
                     capYPositionArray.push(value);
                 };
@@ -201,7 +201,7 @@ Visualizer.prototype = {
                     capYPositionArray[i] = value;
                 };
                 ctx.fillStyle = gradient; //set the filllStyle to gradient for a better look
-                ctx.fillRect(i * 12 /*meterWidth+gap*/ , cheight - value + capHeight, meterWidth, cheight); //the meter
+                ctx.fillRect(i * (meterWidth+gap)+gap, cheight - value + capHeight, meterWidth, cheight); //the meter
             }
             that.animationId = requestAnimationFrame(drawMeter);
         }
